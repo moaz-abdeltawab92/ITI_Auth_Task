d2621202db7b4512dfe24c0207c1207eb17dce7a
MoazAymanAthr <moaza.ayman.athr@gmail.com>
2025-07-24 07:16:54 +0300

    Refactor main app entry point to use MainPage and organize screen imports
    - Updated the main application structure to set MainPage as the home screen.
    - Organized imports by adding LoginScreen, OtpScreen, and SignupScreen for better modularity.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
-import 'package:auth_task2/otp_screen.dart';
+import 'package:auth_task2/pages/login_screen.dart';
+import 'package:auth_task2/pages/otp_screen.dart';
+import 'package:auth_task2/pages/signup_screen.dart';
 import 'package:flutter/material.dart';
+import 'pages/main_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -8,10 +11,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-        debugShowCheckedModeBanner: false, home: OtpScreen());
+      debugShowCheckedModeBanner: false,
+      home: MainPage(),
+    );
   }
 }
